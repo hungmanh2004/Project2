05a8a20f4075d2428a42ede0f95666eb685730ac
hungmanh2004 <139920104+hungmanh2004@users.noreply.github.com>
2025-05-24 00:04:54 +0700

    sửa lại tí cái url

--- MyProject/MyAPI/urls.py
+++ MyProject/MyAPI/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('users/', views.UserView.as_view()),
-    path('users/<int:pk>/', views.UserView.as_view()),
+    path('users/<int:pk>/', views.UserDetailView.as_view()),
     path('posts/', views.PostView.as_view()),
     path('posts/<int:pk>/', views.SinglePostView.as_view()),
     path('comments/', views.CommentView.as_view()),
     path('comments/<int:pk>/', views.CommentDetailView.as_view()),
     path('notifications/', views.NotificationView.as_view()),
     path('notifications/<int:pk>/', views.NotificationDetailView.as_view()),
-]    
+]
